fd7da564bb875d4fc206ab17a76ca7bf9f0cbb7a
raghav <raghavtripurwar2016@gmail.com>
2019-09-12 14:43:19 +0530

    emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,7 @@ var socket = io();
 	socket.on('connect', function(){
 		console.log('connected to server');
 
-		socket.emit('createMessage',{
-			from: 'Raghav',
-			text: 'Hi! raghav',
-		});
+		
 	});
 	socket.on('disconnect',function(){
 		console.log('disconnected from server');
--- server/server.js
+++ server/server.js
@@ -13,13 +13,12 @@ app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
 	console.log('new user connected'); 
 
-	socket.emit('newMessage',{
-		from:"raghav",
-		text:"see you soon",
-		createdAt: 123123,
-	}),
 	socket.on('createMessage',(message)=>{
 		console.log('createMessage',message);
+		io.emit('newMessage',{
+			from: message.from,
+			text: message.text,
+		});
 	});
 
 	socket.on('disconnect',()=>{
