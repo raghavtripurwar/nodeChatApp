bf480914e7c5b04b1705bf86f0ddbc942fc2fc44
raghav <raghavtripurwar2016@gmail.com>
2019-09-12 14:23:00 +0530

    add new message and create message events

---
+++ public/js/index.js
@@ -0,0 +1,16 @@
+var socket = io();
+	socket.on('connect', function(){
+		console.log('connected to server');
+
+		socket.emit('createMessage',{
+			from: 'Raghav',
+			text: 'Hi! raghav',
+		});
+	});
+	socket.on('disconnect',function(){
+		console.log('disconnected from server');
+	});
+
+	socket.on('newMessage',function(message){
+		console.log('newMessage',message);
+	});
--- server/server.js
+++ server/server.js
@@ -13,8 +13,17 @@ app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
 	console.log('new user connected'); 
 
+	socket.emit('newMessage',{
+		from:"raghav",
+		text:"see you soon",
+		createdAt: 123123,
+	}),
+	socket.on('createMessage',(message)=>{
+		console.log('createMessage',message);
+	});
+
 	socket.on('disconnect',()=>{
-		console.log('user was disconnect');
+		console.log('user was disconnected');
 	});
 });
 server.listen(3000,()=>{
