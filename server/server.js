9dd1b77918635966a40d47bf58f26b714b3976e1
raghav <raghavtripurwar2016@gmail.com>
2019-09-12 13:14:00 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const publicPath = path.join(__dirname, '../public');
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+const port = process.env.PORT || 3000;
+var app  = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
+app.use(express.static(publicPath));
+
+io.on('connection',(socket)=>{
+	console.log('new user connected'); 
+});
+server.listen(3000,()=>{
+	console.log(`server is up on ${port}`);
+});
